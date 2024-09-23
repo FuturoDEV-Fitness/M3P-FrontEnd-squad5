d9a83d6147e54606c4e4abd60e5d18bf87f3a4a4
Glauton Osório <glautonleao@gmail.com>
2024-09-23 03:01:45 -0300

    feat:  :art: finalizando o estilo inicial da pagina de login

--- src/components/Forms/Login/index.jsx
+++ src/components/Forms/Login/index.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { InputComponent } from "../../Input";
 import { ButtonComponent } from "../../Button";
+import styles from "./index.module.css";
 
 export const FormLoginComponent = () => {
   const {
@@ -10,59 +11,44 @@ export const FormLoginComponent = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const selectGender = [
-    { value: "", label: "Selecione" },
-    { value: "Male", label: "Masculino" },
-    { value: "Female", label: "Feminino" },
-    { value: "Other", label: "Outro" },
-  ];
 
   return (
-    <div>
-      <div>
-        <div>
-          <span>Login</span>
+    <div className={styles.loginForm}>
+      <h1 className={styles.formTitle}>Login</h1>
+      <form className={styles.Column}>
+        <InputComponent
+          label="Email"
+          type="email"
+          placeholder="email@email"
+          id="emailLogin"
+          register={register("email", {
+            required: "Insira um email válido",
+          })}
+          error={!!errors.email}
+          errorMessage={errors.email?.message}
+        />
 
-          <form>
-            <InputComponent
-              label="Email"
-              type="email"
-              placeholder="email@email"
-              id="emailLogin"
-              register={register("email", {
-                required: "Insira um email válido",
-              })}
-              error={!!errors.email}
-              errorMessage={errors.email?.message}
-            />
+        <InputComponent
+          label="Senha"
+          type="password"
+          placeholder="senha de pelo menos 6 dígitos"
+          id="senhaLogin"
+          register={register("senha", {
+            required: "Insira sua senha",
+            minLength: {
+              value: 6,
+              message: "A senha deve ter no mínimo 6 caracteres",
+            },
+          })}
+          error={!!errors.senha}
+          errorMessage={errors.senha?.message}
+        />
 
-            <InputComponent
-              label="Senha"
-              type="password"
-              placeholder="senha de pelo menos 6 dígitos"
-              id="senhaLogin"
-              register={register("senha", {
-                required: "Insira sua senha",
-                minLength: {
-                  value: 6,
-                  message: "A senha deve ter no mínimo 6 caracteres",
-                },
-              })}
-              error={!!errors.senha}
-              errorMessage={errors.senha?.message}
-            />
-
-            <ButtonComponent
-              variant={"outlined"}
-              type={"submit"}
-              text={"Login"}
-            />
-          </form>
-
-          <span>Não possui conta?</span>
-          <button>SignUp</button>
+        <div className={styles.formColumn}>
+          <span className={styles.styledLink}>Não possui conta?</span>
+          <ButtonComponent variant="outlined" type="submit" text="Login" />
         </div>
-      </div>
+      </form>
     </div>
   );
 };
