d7abbefb5d86231f9888972fc8852d402edcb663
Glauton Osório <glautonleao@gmail.com>
2024-09-26 15:09:11 -0300

    feat: :construction: organização inicial da pagina de dashboard finalizado

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,28 +1,27 @@
+import { MapComponent } from "../../components/Leaflet";
 import styles from "./index.module.css";
-import { useNavigate } from "react-router-dom";
-import CardLista from "../../components/CardLista";
 
 function DashBoard() {
-  const navigate = useNavigate();
-
-  // function contarUsuariosPorEstado() {
-  //     const usuariosPorEstado = {};
-
-  //     for (let estado of ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS',
-  //         'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']) {
-  //         usuariosPorEstado[estado] = 0;
-  //     }
-  //     for (let usuario of usuarios) {
-  //         usuariosPorEstado[usuario.estado]++;
-  //     }
-  //     return usuariosPorEstado;
-  // }
-  // const usuariosPorEstado = contarUsuariosPorEstado();
-
   return (
     <div className={styles.container}>
-      <div className={styles.textual}>
-        <h1>DashBoard</h1>
+      <div className={styles.mapContainer}>
+        <MapComponent />
+        <div className={styles.dashboardContainer}>
+          <span>Numero de locais</span>
+          <span>Numero de usuarios</span>
+          <span>Seus locais cadastrados </span>
+        </div>
+      </div>
+      <div className={styles.cardsContainer}>
+        <div>Card 1</div>
+        <div>Card 2</div>
+        <div>Card 3</div>
+        <div>Card 4</div>
+        <div>Card 5</div>
+        <div>Card 6</div>
+        <div>Card 7</div>
+        <div>Card 8</div>
+        <div>Card 9</div>
       </div>
     </div>
   );
