a7be08f72d5fa06b757fad50f5043a541f8af16f
Glauton Osório <glautonleao@gmail.com>
2024-09-26 17:03:24 -0300

    feat: :hammer: refatoração da rota e pagina de cadastro de locais

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,11 +1,11 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import CadastroLocalExercicio from "../pages/cadastro/index.jsx";
 import PaginaErro from "../pages/PaginaErro";
 import PaginaLista from "../pages/lista/index.jsx";
 import DashBoard from "../pages/dashboard/index.jsx";
 import { PaginaLogin } from "../pages/login/index.jsx";
 import { getLocalStorage } from "../helper/LocalStorageInstance.jsx";
+import { CadastroLocalExercicio } from "../pages/cadastro/index.jsx";
 
 const usuarioLogado = getLocalStorage("user");
 
