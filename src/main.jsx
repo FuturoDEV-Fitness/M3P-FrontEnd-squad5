47711f115421caa990ccea276799f4e1965fc16f
Glauton Osório <glautonleao@gmail.com>
2024-09-28 16:20:09 -0300

    feat: :hammer:  organizando as rotas com o layout e mudando alguns nomes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import routes from "./router/routes";
+import routes from "./router/App.routes.jsx";
 import { AllProviders } from "./AllProviders.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
