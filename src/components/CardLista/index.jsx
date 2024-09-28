fbc0c90c3db24522a323076067c345c4144b5593
Glauton Osório <glautonleao@gmail.com>
2024-09-28 15:40:48 -0300

    feat: :hammer: configurando o card componente

--- src/components/CardLista/index.jsx
+++ src/components/CardLista/index.jsx
@@ -1,42 +1,91 @@
-import styles from "./index.module.css"
-
+import styles from "./index.module.css";
+import { TileLayer, Marker, MapContainer, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+import PropTypes from "prop-types";
 
 function CardLista({ listalocais }) {
+  const customIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+    iconSize: [38, 38],
+  });
 
-    if (!listalocais) {
-        return null; // ou renderizar uma mensagem de erro ou carregamento
-    }
+  return (
+    <div className={styles.cardcontainer}>
+      <MapContainer
+        center={[Number(listalocais.latitude), Number(listalocais.longitude)]}
+        zoom={13}
+        className={styles.mapContainer}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker
+          position={[
+            Number(listalocais.latitude),
+            Number(listalocais.longitude),
+          ]}
+          icon={customIcon}
+        >
+          <Popup />
+        </Marker>
+      </MapContainer>
 
-    return (
-        <div className={styles.cardcontainer}>
+      <div className={styles.topo}>
+        <h3>{listalocais.nomeLocal}</h3>
+        <p>{listalocais.descricaoLocal}</p>
+      </div>
 
-            <div className={styles.topo}>
-                <h3>{listalocais.nomeLocal}</h3>
-                <p><span></span>{listalocais.descricaoLocal}</p>
-            </div>
-            <div className={styles.endereco}>
-                <p><span>Endereço: </span>{listalocais.endereco}</p>
-                <p><span>Bairro: </span>{listalocais.bairro}</p>
-                <p><span>Cidade: </span>{listalocais.cidade}</p>
-                <p><span>Estado: </span>{listalocais.estado}</p>
+      <div className={styles.endereco}>
+        <p>
+          <strong>Endereço:</strong> {listalocais.endereco}
+        </p>
+        <p>
+          <strong>Bairro:</strong> {listalocais.bairro}
+        </p>
+        <p>
+          <strong>Cidade:</strong> {listalocais.cidade}
+        </p>
+        <p>
+          <strong>Estado:</strong> {listalocais.estado}
+        </p>
+      </div>
 
-            </div>
-            <div className={styles.latLong}>
-                <p><span>Coordenadas: </span>{listalocais.latitude} / {listalocais.longitude}</p>
-            </div>
+      <div className={styles.latLong}>
+        <p>
+          <strong>Coordenadas:</strong> {listalocais.latitude} /{" "}
+          {listalocais.longitude}
+        </p>
+      </div>
 
-            <div className={styles.praticas}>
-                <p>Práticas permitidas: {listalocais.praticasEsportivas.map((praticaX, index) => (
-                    <span key={index}>{index == listalocais.praticasEsportivas.length - 1 ? `${" "}${praticaX}.` :
-                        `${" "}${praticaX},`}
-                    </span>
-                ))}</p>
-            </div>
-
-        </div>
-    )
-
-
+      <div className={styles.praticas}>
+        <p>
+          <strong>Práticas permitidas:</strong>
+          {listalocais.praticasEsportivas.map((praticaX, index) => (
+            <span key={index}>
+              {index === listalocais.praticasEsportivas.length - 1
+                ? ` ${praticaX}.`
+                : ` ${praticaX},`}
+            </span>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default CardLista
+export default CardLista;
+
+CardLista.propTypes = {
+  listalocais: PropTypes.shape({
+    nomeLocal: PropTypes.string.isRequired,
+    descricaoLocal: PropTypes.string.isRequired,
+    endereco: PropTypes.string.isRequired,
+    bairro: PropTypes.string.isRequired,
+    cidade: PropTypes.string.isRequired,
+    estado: PropTypes.string.isRequired,
+    latitude: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+      .isRequired,
+    longitude: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+      .isRequired,
+    praticasEsportivas: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
