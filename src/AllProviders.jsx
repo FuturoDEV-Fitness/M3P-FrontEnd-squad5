497d7158f3c7488a6a209861d9b8fdc0950e6c6f
Glauton Osório <glautonleao@gmail.com>
2024-10-02 16:57:01 -0300

    feat: :sparkles: contexto Auth criado para conectar o login com o backend, e refatorando o service de login

--- src/AllProviders.jsx
+++ src/AllProviders.jsx
@@ -4,15 +4,18 @@ import { UsuariosContextProvider } from "./context/UsuariosContext";
 import { LocaisContextProvider } from "./context/LocaisContext";
 import PropTypes from "prop-types";
 import { LoginContextProvider } from "./context/LoginContext";
+import { AuthProvider } from "./context/AuthContext";
 
 export const AllProviders = ({ children }) => {
   return (
     <UsuariosContextProvider>
       <LocaisContextProvider>
-        <LoginContextProvider>
-          <ToastContainer />
-          {children}
-        </LoginContextProvider>
+        <AuthProvider>
+          <LoginContextProvider>
+            <ToastContainer />
+            {children}
+          </LoginContextProvider>
+        </AuthProvider>
       </LocaisContextProvider>
     </UsuariosContextProvider>
   );
